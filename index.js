f0227ab114ad9325c6f9defc5ab95ed793b24d16
Romi58 <romaisanaz67@gmail.com>
2024-05-14 01:18:22 +0500

    update readme.md

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you need to do? in your todo list"
+        },
+        {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more todo?",
+            default: false
+        }
+    ]);
+    const { TODO, addmore } = answers;
+    console.log(answers);
+    loop = addmore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("kindly add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your todo list is :/n");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("no todos found here");
+}
